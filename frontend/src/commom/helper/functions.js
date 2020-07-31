08e821b537a7b996b2996419352e3d09e8e2c26d
richardpassos01 <richard.passos@hotmail.com>
2020-07-31 18:32:19 -0300

    refactor(home): add more styles

--- frontend/src/commom/helper/functions.js
+++ frontend/src/commom/helper/functions.js
@@ -7,26 +7,30 @@ const createDiv = ({
 }) => {
     const div =
         `<div class="product-item" id='product-${id}'>
-            <div class="product-image">
-                <div class="container">
-                    <img class="image"
-                        src="https://apucarana.cidadecancao.com/media/catalog/product/cache/1/image/420x565/9df78eab33525d08d6e5fb8d27136e95/f/1/f131c9be04e7a59a2d8426d40fa48f8825595f.png_2.png"
-                        alt="product-image">
+                <div class="product-image">
+                    <div class="container">
+                        <img class="image"
+                            src="https://apucarana.cidadecancao.com/media/catalog/product/cache/1/image/420x565/9df78eab33525d08d6e5fb8d27136e95/f/1/f131c9be04e7a59a2d8426d40fa48f8825595f.png_2.png"
+                            alt="product-image">
+                    </div>
                 </div>
-            </div>
-            <div class="product-description">
-                <div class="info">
-                    <h2>Coca cola</h2>
-                    <pre>Tem muita açucar</pre>
-                    <span>R$10.00</span>
+                <div class="product-description">
+                    <div class="info-container">
+                        <span class="title">Coca cola</span>
+                        <span class="description">Tem muita açucar</span>
+                        <div class="price">
+                            <span class="currency">R$</span>
+                            <span class="value">10.00</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="product-actions">
-                <div class="itens">
-                    <button id='edit-${id}' onclick='editProduct("edit-${id}")' class="icons"><i
-                            class="fa fa-edit"></i></button>
-                    <button id='delete-${id}' onclick='deleteProduct("product-${id}")' class="icons"><i
-                            class="fa fa-trash-o"></i></button>
+                <div class="product-actions">
+                    <div class="items">
+                        <button id='edit-${id}' onclick='editProduct("edit-${id}")' class="icons"><i
+                                style='font-size:14px' class='far'>&#xf044;</i></button>
+                        <button id='delete-${id}' onclick='deleteProduct("product-${id}")' class="icons"><i
+                                style='font-size:14px' class='far'>&#xf2ed;</i></button>
+                    </div>
                 </div>
             </div>
         </div>
