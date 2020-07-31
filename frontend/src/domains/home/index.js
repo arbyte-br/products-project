abb027562672c00b350222a388bc42e2e9119f96
richardpassos01 <richard.passos@hotmail.com>
2020-07-31 20:42:16 -0300

    feat(home): create simple form inputs

--- frontend/src/domains/home/index.js
+++ frontend/src/domains/home/index.js
@@ -13,4 +13,14 @@ const loadProducts = async () => {
 
 }
 
+const createOrUpdateProduct = () => {
+    const form = document.getElementById('createOrUpdateProduct');
+    const productName = form.elements[0].value;
+    const productPrice = form.elements[1].value;
+    const productDecription = form.elements[2].value;
+    const productCategoryId = form.elements[3].value;
+    const productConver = form.elements[4].value;
+
+}
+
 loadProducts();
