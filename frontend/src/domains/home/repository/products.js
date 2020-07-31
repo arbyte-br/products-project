42b405b9a9a5393e1e8e3ae77c18f53b566d046c
richardpassos01 <richard.passos@hotmail.com>
2020-07-31 19:03:26 -0300

    feat(home): add products

--- frontend/src/domains/home/repository/products.js
+++ frontend/src/domains/home/repository/products.js
@@ -1,8 +1,3 @@
 const fn = () => {
-    const username = 'richardpassos01'
-    axios.get('https://api.github.com/users/' + username)
-        .then(function (response) {
-            console.log(response.data); // ex.: { user: 'Your User'}
-            console.log(response.status); // ex.: 200
-        });
+    // consumo axios
 }
