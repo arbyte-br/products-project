68904788e0b48e6834352fe36b0f897a908af5f1
richardpassos01 <richard.passos@hotmail.com>
2020-07-30 22:04:06 -0300

    feat(home): make a fuck project

--- frontend/src/commom/helper/functions.js
+++ frontend/src/commom/helper/functions.js
@@ -6,10 +6,31 @@ const createDiv = ({
     cover
 }) => {
     const div =
-        `<div id='product-${id}'>
-            <button id='edit-${id}' onclick='editProduct("edit-${id}")'>Editar${id}</button>
-            <button id='delete-${id}' onclick='deleteProduct("product-${id}")'>Deletar${id}</button>
-        </div>`;
+        `<div class="product-item" id='product-${id}'>
+            <div class="product-image">
+                <div class="container">
+                    <img class="image"
+                        src="https://apucarana.cidadecancao.com/media/catalog/product/cache/1/image/420x565/9df78eab33525d08d6e5fb8d27136e95/f/1/f131c9be04e7a59a2d8426d40fa48f8825595f.png_2.png"
+                        alt="product-image">
+                </div>
+            </div>
+            <div class="product-description">
+                <div class="info">
+                    <h2>Coca cola</h2>
+                    <pre>Tem muita açucar</pre>
+                    <span>R$10.00</span>
+                </div>
+            </div>
+            <div class="product-actions">
+                <div class="itens">
+                    <button id='edit-${id}' onclick='editProduct("edit-${id}")' class="icons"><i
+                            class="fa fa-edit"></i></button>
+                    <button id='delete-${id}' onclick='deleteProduct("product-${id}")' class="icons"><i
+                            class="fa fa-trash-o"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
     return div;
 }
